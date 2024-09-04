2cb9d8a2034c781fa86d6c857b2502a9467ef973
Rishabh <rishabh.jha@ACKO1406.local>
2024-09-04 18:02:06 +0530

    Initial commit

--- templatestore/vendor_utils.py
+++ templatestore/vendor_utils.py
@@ -82,10 +82,11 @@ def send_message(message_body, message_attributes=None):
     """
 
     # Create a session using the specified profile
-    session = boto3.Session(profile_name="ackodev")
+    # session = boto3.Session(profile_name="ackodev")
     
     # Create an SQS client using the session
-    sqs = session.client('sqs')
+    # sqs = session.client('sqs')
+    sqs = boto3.client('sqs')
 
     # sqs = boto3.client('sqs', region_name='ap-south-1')
     queue_url = "https://sqs.ap-south-1.amazonaws.com/663498825379/platform-ackommunication-email-p2"
