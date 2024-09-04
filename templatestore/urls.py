21d6a4d2ad467464748d720b090e48df0b71a868
Rishabh <rishabh.jha@ip-192-168-0-103.ap-south-1.compute.internal>
2024-09-04 19:10:47 +0530

    Added fix

--- templatestore/urls.py
+++ templatestore/urls.py
@@ -3,7 +3,7 @@ from templatestore import views
 
 urlpatterns = [
     # apis
-    path("/internal/api/v1/test/abc/abc/abc", views.test_endpoint),
+    path("internal/api/v1/test/abc/abc/abc", views.test_endpoint),
     path("api/v1/template", views.post_template_view),
     path("render_pdf", views.render_pdf),
     path("api/v1/render", views.render_template_view),
